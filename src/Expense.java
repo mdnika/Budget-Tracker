ce8db811008629a64425beb033a7eba8514895b5
Alena <mikhalkevich.a@gmail.com>
2023-06-22 16:21:50 +0200

    feat:add comments to methods and updated file expenses.csv

--- src/Expense.java
+++ src/Expense.java
@@ -28,11 +28,21 @@ public class Expense {
   public String toString() {
     return name + " : " + amount;
   }
-
+  /**
+   * the method takes a sep character as a parameter and returns a formatted string representing
+   * the object's data in CSV (Comma-Separated Values) format.
+   * @param sep breaks the line of text
+   * @return the resulting String
+   */
   public String toCsvString(char sep) {
     return name + sep + amount + '\n';
   }
-
+  /**
+   * the method parses the given line and extract the necessary data to create an Expense object.
+   * @param line represents a text in File CSV
+   * @param sep separator charater
+   * @return the Expsense object
+   */
   public static Expense parseFromCsv(String line, char sep) {
     // line = "Groceries:100.0"
     int sepIndex = line.indexOf(sep);
--- src/ExpenseList.java
+++ src/ExpenseList.java
@@ -19,10 +19,8 @@ public class ExpenseList {
     this.sep = sep;
     expenses = readFromCsv();
   }
-
   /**
    * user input his/her budget
-   *
    * @param scanner to read user input
    * @throws ParseException in case of unparseable input-data
    */
@@ -31,7 +29,10 @@ public class ExpenseList {
     existBudget = scanner.nextDouble();
     scanner.nextLine();
   }
-
+  /**
+   * the writeToCsv() method writes the data of each Expense object in the expenses collection to the specified CSV file
+   * @throws IOException in case there is an error during the file writing process
+   */
   public void writeToCsv() throws IOException {
     FileWriter fileWriter = new FileWriter(file);
     for (Expense expense : expenses) {
@@ -39,35 +40,46 @@ public class ExpenseList {
     }
     fileWriter.close();
   }
-
+  /**
+   * the readFromCsv() method reads data from a CSV file, parses each line into an Expense object,
+   * and returns a list of these objects. Also it handles various exceptions and ignores lines that cannot be parsed correctly
+   * @return the result list, which contains the parsed Expense objects from the CSV file
+   */
   private List<Expense> readFromCsv() {
     List<Expense> result = new ArrayList<>();
     if (!file.exists() || !file.canRead()) {
       return result; // если файла нет или его нельзя читать
     }
     try { // try-catch с ignored, потому что мы заранее всё проверили
-      Scanner scanner = new Scanner(file); // сканер для чтения из файла
+      Scanner scanner = new Scanner(file);
       while (scanner.hasNext()) {
-        String line = scanner.nextLine(); // читаем строку из файла
+        String line = scanner.nextLine();
         try {
-          Expense expense = Expense.parseFromCsv(line, sep); // получаем expense из строки
-          result.add(expense); // добавляем expense в список
+          Expense expense = Expense.parseFromCsv(line, sep);
+          result.add(expense);
         } catch (IndexOutOfBoundsException | NumberFormatException ignored) {
-        } // не получилось прочитать строку
+        }
       }
       scanner.close();
     } catch (FileNotFoundException ignored) {
     }
     return result;
   }
-
+  /**
+   * the print() method prints the details of each expense in the expenses collection, followed by the total budget.
+   */
   public void print() {
     for (Expense expense : expenses) {
       System.out.println(expense);
     }
     System.out.println("Total budget: " + existBudget);
   }
-
+  /**
+   * the given method allows the user to input a new expense and adds it to the expenses list
+   * @param scanner allows the method to read user input or data from keyboard
+   * @throws ParseException throws the exceptions that may occur during user input, for example,
+   * parsing a value, such as when converting a string to a numerical value
+   */
   public void addUserExpense(Scanner scanner) throws ParseException {
     System.out.print("Input the name of your extra expense: ");
     String name = scanner.next();
@@ -81,7 +93,10 @@ public class ExpenseList {
     scanner.nextLine();
     expenses.add(new Expense(name, amount));
   }
-
+  /**
+   * the getTotalExpenses() method accumulates the total amount in the total variable, after iterating through all the expenses
+   * @return the final value of the total variable, which represents the total amount of expenses
+   */
   public double getTotalExpenses() {
     double total = 0;
     for (Expense expense : expenses) {
@@ -89,7 +104,10 @@ public class ExpenseList {
     }
     return total;
   }
-
+  /**
+   * the differenceBudgetExpenses() method calculates the difference between the existing budget
+   * and the total expenses, and then prints the result to the console
+   */
   public void differenceBudgetExpenses() {
     double totalExpenses = getTotalExpenses();
     System.out.println("Total expenses: " + totalExpenses + " EUR");
@@ -100,7 +118,12 @@ public class ExpenseList {
       System.out.println("You have in budget: " + (existBudget - totalExpenses) + " EUR");
     }
   }
-
+  /**
+   * the given method takes a Scanner object as a parameter and returns a string indicating the result of
+   * the deletion operation
+   * @param scanner read user input or data from an input source (keyboard)
+   * @return the result of deleting
+   */
   public String deleteExpense(Scanner scanner) {
     System.out.print("Input an expense to delete: ");
     String name = scanner.next();
@@ -112,7 +135,10 @@ public class ExpenseList {
     }
     return "Expense not found.";
   }
-
+  /**
+   * the addExpense method is responsible for adding an Expense object to the expenses list.
+   * @param expense refers to the Expense object
+   */
   public void addExpense(Expense expense) {
     expenses.add(expense);
   }
